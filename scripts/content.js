b8206c4d66cdad4c87fc68db7b55a65f5f046178
Raphael Quinones <wapquinones28@gmail.com>
2022-12-06 17:00:22 +0800

    injects onto calmly webpage

--- scripts/content.js
+++ scripts/content.js
@@ -8,7 +8,56 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
 })
 
-const insert = () => {
+const insert = (content) => {
+    //get div with classname droid which includes p tag for writing
+    const elements = document.getElementsByClassName('droid');
 
+    if(elements.length === 0) {
+        return;
+    }
+
+    //get topmost div with name droid
+    const element = elements[0]
+
+    //get first p tag to replace it in the top most part of the area
+    const pToRemove = element.childNodes[0]
+    pToRemove.remove();
+
+    //split for every newline to replace it with br later
+    const splitContent = content.split('\n')
+
+    splitContent.forEach((content) => {
+        const p = document.createElement('p')
+
+        //newlines are ''
+        if (content === ''){
+            const br = document.createElement('br')
+            p.appendChild = br
+        } else {
+            p.textContent = content
+        }
+
+        element.appendChild(p)
+    })
+
+
+
+    return true
 }
 
+chrome.runtime.onMessage.addListener(
+    //Listens to message
+    (request, sender, sendResponse) => {
+        if (request.message === 'inject') {
+            const { content } = request
+
+            const result = insert(content);
+
+            if (!result) {
+                sendResponse({ status: 'failed'});
+            }
+
+            sendResponse({status: 'success'})
+        }
+    }
+)
