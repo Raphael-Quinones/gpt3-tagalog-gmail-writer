ade11375067699718c20893c46f4284c8d22809c
Raphael Quinones <wapquinones28@gmail.com>
2023-01-08 01:43:47 +0800

    added limit feature

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -1,3 +1,9 @@
+// Create a counter to keep track of the number of clicks
+let clickCounter = 0;
+
+// Specify the maximum number of clicks allowed
+const MAX_CLICKS = 2;
+
 const getKey = () => {
   return new Promise((resolve, reject) => {
     chrome.storage.local.get(['openai-key'], (result) => {
@@ -48,36 +54,47 @@ const generate = async (prompt) => {
   return completion.choices.pop();
 }
 
+
+
 const generateCompletionAction = async (info) => {
 	try {
-		// Send mesage with generating text (this will be like a loading indicator)
-		sendMessage('generating...');
-	
-    const { selectionText } = info;
-    const basePromptPrefix =
-		`
-		Write me a detailed table of contents for an email with the topic below.
-
-		Topic:
-		`;
-
-    const baseCompletion = await generate(`${basePromptPrefix}${selectionText}`);
- 
-	const secondPrompt = 
-	  `
-	  Take the table of contents and topic of the email below and generate a full formal email. .
-
-	  Topic: ${selectionText}
-
-	  Table of Contents: ${baseCompletion.text}
-
-	  Email:
-	  `;
-		
-    const secondPromptCompletion = await generate(secondPrompt);
-		
-	// Send the output when we're all done
-	sendMessage(secondPromptCompletion.text);
+      if (clickCounter >= MAX_CLICKS) {
+        //Send warning that they have reached max number of clicks
+        console.log("You have reached max number of clicks")
+    }
+    else{
+            //Increment ClickCounter
+            clickCounter++;
+            console.log("Clicks total: ", clickCounter)
+            // Send mesage with generating text (this will be like a loading indicator)
+            sendMessage('generating...');
+    
+            const { selectionText } = info;
+            const basePromptPrefix =
+            `
+            Write me a detailed table of contents for an email with the topic below.
+      
+            Topic:
+            `;
+      
+            const baseCompletion = await generate(`${basePromptPrefix}${selectionText}`);
+        
+          const secondPrompt = 
+            `
+            Take the table of contents and topic of the email below and generate a full formal email. .
+      
+            Topic: ${selectionText}
+      
+            Table of Contents: ${baseCompletion.text}
+      
+            Email:
+            `;
+            
+            const secondPromptCompletion = await generate(secondPrompt);
+            
+          // Send the output when we're all done
+          sendMessage(secondPromptCompletion.text);
+    }
   } catch (error) {
     console.log(error);
 
